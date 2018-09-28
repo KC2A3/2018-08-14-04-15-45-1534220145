24a6bf6d67a330ba6226d43fb0be012333bede02
KC2A3 <947496351@qq.com>
2018-09-28 21:52:59 +0800

    修改n次后

--- main/main.js
+++ main/main.js
@@ -1,4 +1,11 @@
 // Write your cade below:
-module.exports = function main() {
-  
+function Mymethod(x,y) {
+	if(y===0) {
+		return '无法计算'
+	} else {
+		return x%y;
+	} 
 };
+module.exports = {
+	Mymethod
+}
--- spec/main-spec.js
+++ spec/main-spec.js
@@ -3,7 +3,7 @@ const main = require('../main/main');
 describe('main()', () => {
 
     it('should calculate the remaindar', () => {
-        expect(main(9, 3)).toBe(0);
-        expect(main(11, 4)).toBe(3);
+        expect(main.Mymethod(9, 3)).toBe(0);
+        expect(main.Mymethod(11, 4)).toBe(3);
     });
 });
